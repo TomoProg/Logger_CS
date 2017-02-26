4b56b48ff1a9ea75c35ed5b29c52e7ddc1ed1392
TomoProg <helloworld0306.xxx@gmail.com>
2017-02-26 19:43:48 +0900

    ユニットテストプロジェクト追加

---
+++ Logger/LoggerUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LoggerUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LoggerUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3b60071-ffab-405c-b175-6ea63f227b50")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Logger/LoggerUnitTest/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+using System.IO;
+using MyUtility;
+
+namespace LoggerUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// ファイルの最終行に指定した内容が書き込まれているかをテストする
+        /// </summary>
+        [TestMethod]
+        public void TestFileContents()
+        {
+            string fileMakePath = "test.txt";
+            string writeContents = "TestMethod1";
+            Logger l = new Logger(fileMakePath, Encoding.UTF8);
+
+            l.Write(writeContents);
+            
+            string[] fileContents = File.ReadAllLines(fileMakePath);
+            string lastLine = fileContents[fileContents.Length - 1];
+
+            Func<string, string, bool> isExistString = 
+                (line, searchString) => { return line.IndexOf(searchString) >= 0 ? true : false; };
+
+            Assert.AreEqual(expected: true, actual: isExistString(lastLine, writeContents));
+            Assert.AreEqual(expected: false, actual: isExistString(lastLine, "TestMethod2"));
+        }
+
+        /// <summary>
+        /// ファイルが作成されているかをテストする
+        /// </summary>
+        [TestMethod]
+        public void TestMakeFile()
+        {
+            string fileMakePath = "test.txt";
+            string writeContents = "TestMethod1";
+            Logger l = new Logger(fileMakePath, Encoding.UTF8);
+            
+            if (File.Exists(fileMakePath))
+            {
+                File.Delete(fileMakePath);
+            }
+            l.Write(writeContents);
+
+            Assert.AreEqual(expected: true, actual: File.Exists(fileMakePath));
+        }
+    }
+}
